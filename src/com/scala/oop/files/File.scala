b7f6bf8f2980b508c7125f7ec2f401a5212ce215
piotrswiderski <piotrs1999@gmail.com>
2021-08-25 21:03:33 +0200

    cat added

--- src/com/scala/oop/files/File.scala
+++ src/com/scala/oop/files/File.scala
@@ -2,7 +2,7 @@ package com.scala.oop.files
 
 import com.scala.oop.filesystem.FileSystemException
 
-class File(override val parentPath: String, override val name: String, contents: String) extends  DirEntry(parentPath, name){
+class File(override val parentPath: String, override val name: String, val contents: String) extends  DirEntry(parentPath, name){
 
   def asDirectory: Directory =
     throw new FileSystemException("A file cannot be converted to a directory")
--- src/com/scala/oop/filesystem/FileSystem.scala
+++ src/com/scala/oop/filesystem/FileSystem.scala
@@ -7,9 +7,15 @@ import java.util.Scanner
 
 object FileSystem extends App {
 
-  val scanner = new Scanner(System.in)
-
   val root = Directory.ROOT
+
+  io.Source.stdin.getLines().foldLeft(State(root, root)) ((currentState, newLine) => {
+    currentState.show()
+    Command.from(newLine).apply(currentState)
+  })
+
+  /*
+  val scanner = new Scanner(System.in)
   var state = State(root, root)
 
   while(true){
@@ -17,5 +23,5 @@ object FileSystem extends App {
     val input = scanner.nextLine()
     state = Command.from(input).apply(state)
   }
-
+  */
 }
